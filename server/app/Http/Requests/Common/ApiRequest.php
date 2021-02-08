0225c1ccb52b62f1937aede8beb9d4deda4b1b31
Danh Nguyễn <nguyentd92@gmail.com>
2021-02-09 01:22:19 +0700

    Extend ApiRequest.php With Failed Validation Methods from built-in FormRequest

---
+++ server/app/Http/Requests/Common/ApiRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\Http\Requests\Common;
+
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+
+class ApiRequest extends FormRequest
+{
+    public function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+        throw new HttpResponseException(response()->json(
+            [
+                'error' => $errors,
+                'status_code' => 422,
+            ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY));
+    }
+}
