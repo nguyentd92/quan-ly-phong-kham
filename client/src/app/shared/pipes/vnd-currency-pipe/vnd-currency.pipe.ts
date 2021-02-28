de85c295e961a5ea3249aa40f989249f076ac4b7
Nguyễn Danh <nguyentd92@gmail.com>
2021-03-01 00:15:30 +0700

    Fĩ Pipe

--- client/src/app/shared/pipes/vnd-currency-pipe/vnd-currency.pipe.ts
+++ client/src/app/shared/pipes/vnd-currency-pipe/vnd-currency.pipe.ts
@@ -2,11 +2,11 @@ import { CurrencyPipe } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'vnd-currency'
+  name: 'vndCurrency'
 })
 export class VndCurrencyPipe extends CurrencyPipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    return super.transform(value, 'VND').replaceAll(',', '.');
+    return super.transform(value, 'VND').replace(/,/g, '.');
   }
 }
