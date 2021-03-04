00a0f0aae0ca8f2a03072d0226d254ec64d27378
Nguyễn Danh <nguyentd92@gmail.com>
2021-03-04 12:10:24 +0700

    Add class container View Prescription Component

--- client/src/app/prescription/popups/view-prescription/view-prescription.component.ts
+++ client/src/app/prescription/popups/view-prescription/view-prescription.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-view-prescription',
   templateUrl: './view-prescription.component.html',
-  styleUrls: ['./view-prescription.component.scss']
+  styleUrls: ['./view-prescription.component.scss'],
+  host: {
+    "class": "container"
+  }
 })
 export class ViewPrescriptionComponent implements OnInit {
 
