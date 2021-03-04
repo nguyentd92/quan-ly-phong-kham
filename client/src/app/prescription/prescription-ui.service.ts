494d35a44daa7802f2a7a971fc280188a37ddff2
Nguyễn Danh <nguyentd92@gmail.com>
2021-03-04 11:48:09 +0700

    Add Mode Open Create Prescription with ReExam Mode

--- client/src/app/prescription/prescription-ui.service.ts
+++ client/src/app/prescription/prescription-ui.service.ts
@@ -4,6 +4,7 @@ import { NzDrawerService } from "ng-zorro-antd/drawer";
 import { NzModalService } from "ng-zorro-antd/modal";
 import { Observable } from "rxjs";
 import { Patient } from '../shared/models/patient.model';
+import { Prescription } from '../shared/models/prescription.model';
 import { CreatePrescriptionComponent } from "./popups/create-prescription/create-prescription.component";
 import { SearchPrescriptionComponent } from "./popups/search-prescription/search-prescription.component";
 import { ViewPrescriptionComponent } from './popups/view-prescription/view-prescription.component';
@@ -30,18 +31,19 @@ export class PrescriptionUIService {
     return modal.afterClose;
   }
 
-  openCreatePrescriptionDrawer(patient: Partial<Patient> = null): Observable<any> {
+  openCreatePrescriptionDrawer(patient: Partial<Patient> = null, fromPrescription: Partial<Prescription> = null): Observable<any> {
     const patientInfo: string = patient ? `${patient?.full_name} - Tel: ${patient?.phone} - Đc: ${patient?.address}` : "";
     const drawerRef = this.nzDrawerService.create<CreatePrescriptionComponent, { value: string }, string>({
       nzTitle: `<div class="container">
-          Tạo phiếu khám bệnh: ${patientInfo}
+          Tạo phiếu ${!fromPrescription ? "khám bệnh" : "tái khám"}: ${patientInfo}
         </div>
       `,
       nzContent: CreatePrescriptionComponent,
       nzPlacement: 'bottom',
       nzHeight: '98vh',
       nzContentParams: {
-        patient
+        patient,
+        fromPrescription
       }
     });
 
