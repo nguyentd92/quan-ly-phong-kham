eef57dc05bf0cbea0fb24a39b68a7c2bad71dafb
Nguyễn Danh <nguyentd92@gmail.com>
2021-03-05 08:38:14 +0700

    Declare Patients Type, Patient Model, Add some Option Props

--- client/src/app/shared/models/patient.model.ts
+++ client/src/app/shared/models/patient.model.ts
@@ -7,7 +7,13 @@ export class Patient {
   address: string;
   guardian: string;
 
-  gender_str: string;
-  age_str: string;
-  dob_str: string;
+  gender_str?: string;
+  age_str?: string;
+  dob_str?: string;
+
+  age_int?: number;
+
+  last_exam_at?: Date;
 }
+
+export type Patients = Patient[];
