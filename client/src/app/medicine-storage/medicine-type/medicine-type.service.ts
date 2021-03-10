7cf5201bba95d1f9e614fe0d9b96b2484d46f95b
Nguyễn Danh <nguyentd92@gmail.com>
2021-03-10 21:48:49 +0700

    MedicineTypeService: Implement Method getMedicineTypes

--- client/src/app/medicine-storage/medicine-type/medicine-type.service.ts
+++ client/src/app/medicine-storage/medicine-type/medicine-type.service.ts
@@ -13,10 +13,32 @@ export class MedicineTypeService {
   constructor(private uiMessageService: UIMessageService) { }
 
   public createMedicineType(req: CreateMedicineTypeRequest): Observable<Medication> {
-
     return of({
       id: 10,
       ...req
     }).pipe(delay(1000), tap(e => this.uiMessageService.success("Thêm nhóm thuốc thành công")));
+  }
+
+  public getMedicineTypes(): Observable<Medication[]> {
+    return of([
+      {
+        id: 1,
+        name: "Medicine 1"
+      },
+      {
+        id: 2,
+        name: "Medicine 2",
+        description: "Description"
+      },
+      {
+        id: 3,
+        name: "Medicine 3",
+        description: "Description"
+      },
+      {
+        id: 4,
+        name: "Medicine 4"
+      },
+    ]).pipe(delay(1000))
   }
 }
