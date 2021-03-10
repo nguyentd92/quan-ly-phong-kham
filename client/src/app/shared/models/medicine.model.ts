55f1132495793a437d85e747b370bde042e9171c
Nguyễn Danh <nguyentd92@gmail.com>
2021-03-10 22:30:59 +0700

    Fix Model Medicine

--- client/src/app/shared/models/medicine.model.ts
+++ client/src/app/shared/models/medicine.model.ts
@@ -1,13 +1,24 @@
 import {MedicineUnit} from "./medicine-unit.model";
 import {Medication} from "./medication.model";
+import { MedicineUnitSmall } from "./medicine-unit-small.model";
 
 export class Medicine {
   id: number;
   name: string;
   unit_sell_price: number;
   in_stocks: number;
-  unit_vol: string;
-  med_unit?: MedicineUnit;
-  medication?: Medication;
+
+  unit_small_id: number;
+  unit_large_id: number;
+
+  unit_vol_small: string;
+  unit_vol_large: string;
+
+  medication_id: number;
+
   images?: string[];
+
+  medication?: Medication;
+  med_unit_small?: MedicineUnitSmall;
+  med_unit_large?: MedicineUnit;
 }
