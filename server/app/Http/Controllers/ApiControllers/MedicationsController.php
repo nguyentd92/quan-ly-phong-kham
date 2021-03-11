fe426ca8929e00256f7ac4e512590d73d65bc0bc
Danh Nguyễn <nguyentd92@gmail.com>
2021-03-11 09:54:00 +0700

    Implement Create Medication Action

--- server/app/Http/Controllers/ApiControllers/MedicationsController.php
+++ server/app/Http/Controllers/ApiControllers/MedicationsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\ApiControllers;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Medications\UpsertMedicationRequest;
 use App\Repositories\Contracts\MedicationRepository;
 use Illuminate\Http\JsonResponse;
 
@@ -18,5 +19,14 @@ class MedicationsController extends Controller
 
     public function getAll(): JsonResponse {
         return response()->json($this->medicationRepository->getList());
+    }
+
+    public function store(UpsertMedicationRequest $request): JsonResponse {
+        $entity = $this->medicationRepository->store([
+            "medication_name" => $request->name,
+            "medication_des" => $request->description
+        ]);
+
+        return response()->json($entity, JsonResponse::HTTP_OK);
     }
 }
