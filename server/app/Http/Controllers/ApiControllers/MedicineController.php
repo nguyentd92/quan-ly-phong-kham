a28f49d34aea5819167afbf3621623490fb3b701
Danh Nguyễn <nguyentd92@gmail.com>
2021-03-11 14:41:00 +0700

    Implement Medicine Controller Store

--- server/app/Http/Controllers/ApiControllers/MedicineController.php
+++ server/app/Http/Controllers/ApiControllers/MedicineController.php
@@ -3,13 +3,12 @@
 namespace App\Http\Controllers\ApiControllers;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Medicines\UpsertMedicineRequest;
 use App\Repositories\Contracts\MedicineRepository;
 use Illuminate\Http\JsonResponse;
 
 class MedicineController extends Controller
 {
-    //
     protected $medicineRepository;
 
     public function __construct(MedicineRepository $medicineRepository)
@@ -19,5 +18,18 @@ class MedicineController extends Controller
 
     public function getAll(): JsonResponse {
         return response()->json($this->medicineRepository->getList());
+    }
+
+    public function store(UpsertMedicineRequest $request): JsonResponse {
+        $entity = $this->medicineRepository->store([
+            'med_name' => $request->name,
+            'unit_sell_price' => $request->unit_sell_price,
+            'unit_sm_vol' => $request->unit_sm_vol,
+            'unit_sm_id' => $request->unit_sm_id,
+            'images' => $request->images,
+            'medication_id' => $request->medication_id,
+        ]);
+
+        return response()->json($entity, JsonResponse::HTTP_CREATED);
     }
 }
