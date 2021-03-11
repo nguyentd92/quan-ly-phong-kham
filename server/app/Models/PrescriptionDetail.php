b51a7966a0a9bb3954eaa4145005d8a6faab3d03
Nguyễn Danh <nguyentd92@gmail.com>
2021-03-11 22:54:45 +0700

    PrescriptionDetail Model: Add Fillable

--- server/app/Models/PrescriptionDetail.php
+++ server/app/Models/PrescriptionDetail.php
@@ -6,7 +6,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class PrescriptionDetails extends Model
+class PrescriptionDetail extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        "pres_id",
+        "med_id",
+        "formulae",
+        "amount",
+        "unit_sell_price",
+        "sell_price"
+    ];
 }
