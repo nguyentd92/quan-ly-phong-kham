85950f21d685397bd4767ca491a4521c71b8e712
Nguyễn Danh <nguyentd92@gmail.com>
2021-03-11 22:57:37 +0700

    Implement Prescription Model: primaryKey & fillable

--- server/app/Models/Prescription.php
+++ server/app/Models/Prescription.php
@@ -9,4 +9,17 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Prescription extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $primaryKey = "pres_id";
+
+    protected $fillable = [
+        "symptoms",
+        "diagnosis",
+        "pres_price",
+        "medication_price",
+        "bill_price",
+        "patient_id",
+        "re_exam_to",
+        "note"
+    ];
 }
