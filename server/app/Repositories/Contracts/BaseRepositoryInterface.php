2065f2bc337012a0d662c149c416620a7b0fc081
Nguyễn Danh <nguyentd92@gmail.com>
2021-03-11 22:38:32 +0700

    Add Abstract Method insertBulk

--- server/app/Repositories/Contracts/BaseRepositoryInterface.php
+++ server/app/Repositories/Contracts/BaseRepositoryInterface.php
@@ -9,6 +9,7 @@ interface BaseRepositoryInterface
     function getList();
     function getSingle($id);
     function store(array $attributes);
+    function insertBulk(array $listAttributes);
     function update($id, array $attributes);
     function delete($id);
 }
